bc8a50b74efc9fbc46ea4f0b800f8a61a0d0b3b0
1Rendes <1admin@ukr.net>
2024-03-22 16:48:58 +0100

    mobile-menu add

---
+++ src/main.js
@@ -0,0 +1,8 @@
+function MenuOpen() {
+  var navbar = document.getElementById('backdrop');
+  navbar.classList.add('is-open');
+}
+function MenuClose() {
+  var navbar = document.getElementById('backdrop');
+  navbar.classList.remove('is-open');
+}
