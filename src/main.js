1039c467cf22c1fc45770c7d2bbeed717d7ffc5f
1Rendes <1admin@ukr.net>
2024-03-22 19:06:52 +0100

    menu + script update

--- src/main.js
+++ src/main.js
@@ -1,8 +1,1 @@
-function MenuOpen() {
-  var navbar = document.getElementById('backdrop');
-  navbar.classList.add('is-open');
-}
-function MenuClose() {
-  var navbar = document.getElementById('backdrop');
-  navbar.classList.remove('is-open');
-}
+
