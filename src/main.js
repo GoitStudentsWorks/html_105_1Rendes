69cc222caf6b4dd18d802d174381915c9f499a60
1Rendes <1admin@ukr.net>
2024-03-23 13:29:51 +0100

    test js

--- src/main.js
+++
@@ -1,7 +0,0 @@
-function openNav() {
-  document.getElementById('backdrop').classList.add('is-open');
-}
-
-function closeNav() {
-  document.getElementById('backdrop').classList.remove('is-open');
-}
